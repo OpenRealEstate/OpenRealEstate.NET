e61d7754ebd80b695872297f7353d927c671ce2d
Pure Krome <pure.krome@world-domination.com.au>
2014-07-18 00:01:47 +1000

    :lipstick:

--- Code/OpenRealEstate.Core/Models/Rental/RentalListing.cs
+++ Code/OpenRealEstate.Core/Models/Rental/RentalListing.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace OpenRealEstate.Core.Models.Rental
 {
@@ -8,27 +7,5 @@ namespace OpenRealEstate.Core.Models.Rental
         public PropertyType PropertyType { get; set; }
         public DateTime? AvailableOn { get; set; }
         public RentalPricing Pricing { get; set; }
-
-        public new void Validate(Dictionary<string, string> errors, string keySuffix = null)
-        {
-            if (errors == null)
-            {
-                throw new ArgumentNullException("errors");
-            }
-
-            // Each key needs to be determined by the Agency + Unique id. This way,
-            // when we are validating multiple listings, we don't get a duplicate key-conflict.
-            if (string.IsNullOrWhiteSpace(keySuffix))
-            {
-                keySuffix = string.Empty;
-            }
-
-            //base.Validate(errors, keySuffix);
-
-            if (PropertyType == PropertyType.Unknown)
-            {
-                errors.Add("PropertyType" + keySuffix, "Invalid PropertyType. Please choose any property except Unknown.");
-            }
-        }
     }
 }
