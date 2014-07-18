d33a420e92afe7e81e47285799d83c7c34067fb7
Pure Krome <pure.krome@world-domination.com.au>
2014-07-18 13:49:05 +1000

    Fix a busted unit test.

--- Code/OpenRealEstate.Tests/Validators/Rental/RentalListingValidatorFacts.cs
+++ Code/OpenRealEstate.Tests/Validators/Rental/RentalListingValidatorFacts.cs
@@ -43,10 +43,10 @@ namespace OpenRealEstate.Tests.Validators.Rental
 
         public class ValidationFacts
         {
-            private static RentalListing CreateListing()
+            private static RentalListing CreateListing(string fileName = null)
             {
-                var xml =
-                    File.ReadAllText("Sample Data\\Transmorgrifiers\\REA\\Rental\\REA-Rental-Current.xml");
+                var xml = File.ReadAllText(fileName
+                                           ?? "Sample Data\\Transmorgrifiers\\REA\\Rental\\REA-Rental-Current.xml");
                 var transmogrifier = new ReaXmlTransmorgrifier();
                 return transmogrifier.ConvertTo(xml).Listings.First().Listing as RentalListing;
             }
@@ -76,12 +76,15 @@ namespace OpenRealEstate.Tests.Validators.Rental
                     ruleSet: RentalListingValidator.MinimumRuleSet);
 
                 // Assert.
-                result.Errors.Count.ShouldBe(7);
+                result.Errors.Count.ShouldBe(8);
                 result.Errors.ShouldContain(x => x.PropertyName == "AgencyId");
                 result.Errors.ShouldContain(x => x.PropertyName == "StatusType");
                 result.Errors.ShouldContain(x => x.PropertyName == "CreatedOn");
                 result.Errors.ShouldContain(x => x.PropertyName == "Id");
                 result.Errors.ShouldContain(x => x.PropertyName == "UpdatedOn");
+                result.Errors.ShouldContain(x => x.PropertyName == "Title");
+                result.Errors.ShouldContain(x => x.PropertyName == "Description");
+                result.Errors.ShouldContain(x => x.PropertyName == "Address");
             }
         }
     }
