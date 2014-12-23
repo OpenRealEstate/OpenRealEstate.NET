a40ef2545eabb81f0989698cd74fe8d9bb919575
Pure Krome <github@world-domination.com.au>
2014-12-23 11:21:08 +1100

    :lipstick:

--- Code/OpenRealEstate.Validation/AddressValidator.cs
+++ Code/OpenRealEstate.Validation/AddressValidator.cs
@@ -9,34 +9,31 @@ namespace OpenRealEstate.Validation
 
         public AddressValidator()
         {
-            //RuleSet(RuleLocationNames, () =>
-            //{
-                // Cannot have a street number/lot and no street name.
-                RuleFor(address => address.Street).NotEmpty()
-                    .When(address => !string.IsNullOrWhiteSpace(address.StreetNumber))
-                    .WithMessage("A street name is required when a street number has been provided. Eg. Smith Street.");
+            // Cannot have a street number/lot and no street name.
+            RuleFor(address => address.Street).NotEmpty()
+                .When(address => !string.IsNullOrWhiteSpace(address.StreetNumber))
+                .WithMessage("A street name is required when a street number has been provided. Eg. Smith Street.");
 
-                RuleFor(address => address.Suburb).NotEmpty()
-                    .WithMessage("A Suburb is required. Eg. Ivanhoe or Pott's Point.");
+            RuleFor(address => address.Suburb).NotEmpty()
+                .WithMessage("A Suburb is required. Eg. Ivanhoe or Pott's Point.");
 
-                RuleFor(address => address.State).NotEmpty()
-                    .WithMessage("A State is required. Eg. Victoria or New South Wales.");
+            RuleFor(address => address.State).NotEmpty()
+                .WithMessage("A State is required. Eg. Victoria or New South Wales.");
 
-                RuleFor(address => address.CountryIsoCode).NotEmpty()
-                    .WithMessage("A Country ISO code is required. eg. AU, NZ, etc.");
+            RuleFor(address => address.CountryIsoCode).NotEmpty()
+                .WithMessage("A Country ISO code is required. eg. AU, NZ, etc.");
 
-                RuleFor(address => address.Latitude)
-                    .GreaterThanOrEqualTo(-90M)
-                    .LessThanOrEqualTo(90M)
-                    .When(address => address.Latitude.HasValue)
-                        .WithMessage("Latitude value has to be between -90 and 90.");
+            RuleFor(address => address.Latitude)
+                .GreaterThanOrEqualTo(-90M)
+                .LessThanOrEqualTo(90M)
+                .When(address => address.Latitude.HasValue)
+                .WithMessage("Latitude value has to be between -90 and 90.");
 
-                RuleFor(address => address.Longitude)
-                        .GreaterThanOrEqualTo(-180M)
-                        .LessThanOrEqualTo(180M)
-                        .When(address => address.Longitude.HasValue)
-                            .WithMessage("Longitude value has to be between -180 and 180.");
-            //});
+            RuleFor(address => address.Longitude)
+                .GreaterThanOrEqualTo(-180M)
+                .LessThanOrEqualTo(180M)
+                .When(address => address.Longitude.HasValue)
+                .WithMessage("Longitude value has to be between -180 and 180.");
         }
     }
 }
