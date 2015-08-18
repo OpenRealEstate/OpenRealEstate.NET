70f2c5323c41fc6bd4c40c9414b07d68eb7ef253
Pure Krome <github@world-domination.com.au>
2015-08-18 14:38:20 +1000

    Flipped parameter order for ConvertTo(..).

--- Code/OpenRealEstate.Services/RealEstateComAu/ReaXmlTransmorgrifier.cs
+++ Code/OpenRealEstate.Services/RealEstateComAu/ReaXmlTransmorgrifier.cs
@@ -31,12 +31,12 @@ namespace OpenRealEstate.Services.RealEstateComAu
         /// Converts some given data into a listing instance.
         /// </summary>
         /// <param name="data">some data source, like Xml data or json data.</param>
-        /// <param name="isClearAllIsModified">After the data is loaded, do we clear all IsModified fields so it looks like the listing(s) are all ready to be used and/or compared against other listings.</param>
         /// <param name="areBadCharactersRemoved">Help clean up the data.</param>
-        /// <returns>List of listings and any unhandled data.</returns>
+        /// <param name="isClearAllIsModified">After the data is loaded, do we clear all IsModified fields so it looks like the listing(s) are all ready to be used and/or compared against other listings.</param>
+        /// /// <returns>List of listings and any unhandled data.</returns>
         public ConvertToResult ConvertTo(string data, 
-            bool isClearAllIsModified = false,
-            bool areBadCharactersRemoved = false)
+            bool areBadCharactersRemoved = false,
+            bool isClearAllIsModified = false)
         {
             data.ShouldNotBeNullOrEmpty();
 
