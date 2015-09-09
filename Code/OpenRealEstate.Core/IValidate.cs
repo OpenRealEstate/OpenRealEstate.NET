9ea67b9a25b4d9ff4f4369603baa44ec8fd26727
Pure Krome <github@world-domination.com.au>
2015-09-09 15:02:16 +1000

    :put_litter_in_its_place:

--- Code/OpenRealEstate.Core/IValidate.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace OpenRealEstate.Core
-{
-    public interface IValidate
-    {
-        void Validate(Dictionary<string, string> errors, string keySuffix = null);
-    }
-}
